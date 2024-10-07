b54a1f8c4e6a18d2e6fc44d885632bb0cb0d0cc2
ScurryTurry <92820704+ScurryTurry@users.noreply.github.com>
2024-10-07 14:43:22 -0400

    nested loops

--- src/Main.java
+++ src/Main.java
@@ -7,32 +7,66 @@ public class Main {
         Scanner in = new Scanner(System.in);
         Random gen = new Random();
 
-            for(int c = 0; c < 31; c++)
-            {
-                System.out.print(c + " ");
-            }
+        for(int c = 0; c < 31; c++)
+        {
+            System.out.print(c + " ");
+        }
 
         System.out.println("\n");
 
-            for(int c = 30; c > 0; c--)
-            {
-                System.out.print(c + " ");
-            }
+        for(int c = 30; c > 0; c--)
+        {
+            System.out.print(c + " ");
+        }
 
         System.out.println("\n");
 
-            for(int c = 0; c < 19; c += 3)
-            {
-                System.out.print(c + " ");
-            }
+        for(int c = 0; c < 19; c += 3)
+        {
+            System.out.print(c + " ");
+        }
 
         System.out.println("\n");
 
-            for(int c = 10; c >= 0; c -= 2)
-            {
-                System.out.print(c + " ");
-            }
+        for(int c = 10; c >= 0; c -= 2)
+        {
+            System.out.print(c + " ");
+        }
 
         System.out.println("\n");
+
+        for(int row = 1; row <= 5; row++)
+        {
+            for(int col = 1; col <= row; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        for(int row = 5; row >= 1; row--)
+        {
+            for(int col = 1; col <= row; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        for(int row = 1; row <= 5; row++)
+        {
+            for(int col = 1; col <= 5; col++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
     }
 }
