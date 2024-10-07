8b05344818734df8abcd72df7688dfc4945ed431
ScurryTurry <92820704+ScurryTurry@users.noreply.github.com>
2024-10-07 14:21:35 -0400

    for loops

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        Random gen = new Random();
+
+            for(int c = 0; c < 31; c++)
+            {
+                System.out.print(c + " ");
+            }
+
+        System.out.println("\n");
+
+            for(int c = 30; c > 0; c--)
+            {
+                System.out.print(c + " ");
+            }
+
+        System.out.println("\n");
+
+            for(int c = 0; c < 19; c += 3)
+            {
+                System.out.print(c + " ");
+            }
+
+        System.out.println("\n");
+
+            for(int c = 10; c >= 0; c -= 2)
+            {
+                System.out.print(c + " ");
+            }
+
+        System.out.println("\n");
     }
 }
